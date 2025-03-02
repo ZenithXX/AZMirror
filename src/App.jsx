af12b910206a94b16bae90918352d281a5f2d4da
ZenithXX <vitit.c@kkumail.com>
2025-03-03 02:23:21 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ModelViewer from "./components/ModelViewer";
+
+function App() {
+  return <ModelViewer modelPath="/models/az.glb" />;
+  //   return <ModelViewer modelPath="/models/GLTF/az.gltf" />;
+}
+
+export default App;
---
+++ src/components/ModelViewer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment, useGLTF } from "@react-three/drei";
+
+function Model({ modelPath }) {
+  const { scene } = useGLTF(modelPath);
+  return <primitive object={scene} scale={2} />;
+}
+
+export default function ModelViewer({ modelPath }) {
+  return (
+    <Canvas
+      camera={{ position: [0, 2, 5], fov: 50 }}
+      style={{ width: "100vw", height: "100vh" }}
+      shadowscamera={{ position: [0, 2, 5], fov: 50 }}
+    >
+      <ambientLight intensity={1} />
+      <directionalLight position={[5, 5, 2]} intensity={1.5} castShadow />
+      <directionalLight position={[-5, -5, -2]} intensity={0.8} />
+
+      <Environment preset="studio" />
+      <Model modelPath={modelPath} />
+      <OrbitControls enableZoom={true} />
+    </Canvas>
+  );
+}
